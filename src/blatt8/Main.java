81aa601038dae5e40ed08f29067e58c3e6dfb109
justayak <justayak@googlemail.com>
2013-06-07 18:48:58 +0200

    init commit

---
+++ src/blatt8/Main.java
@@ -0,0 +1,15 @@
+package blatt8;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Baka
+ * Date: 07.06.13
+ * Time: 18:47
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+
+    public static void main(String[]args){
+
+    }
+}
---
+++ src/blatt8/Parser.java
@@ -0,0 +1,11 @@
+package blatt8;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Baka
+ * Date: 07.06.13
+ * Time: 18:48
+ * To change this template use File | Settings | File Templates.
+ */
+public class Parser {
+}
